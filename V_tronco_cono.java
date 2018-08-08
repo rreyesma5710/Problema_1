7f24115ec5d6e57ff0530a55bce5e0d9c7b2d997
Rolando Reyes Martinez <rreyesma5710@universidadean.edu.co>
2018-08-07 22:44:35 -0500

    Actualización

---
+++ V_tronco_cono.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class V_tronco_cono here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class V_tronco_cono
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class V_tronco_cono
+     */
+    public V_tronco_cono()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
